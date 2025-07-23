f3cba05d699990a73ab31e4d677d435b40c25bbb
sayujkuickel <sayujkuickel@gmail.com>
2025-07-23 22:52:48 +0545

    basic setup upload

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
+    plugins: [tailwindcss()],
+  },
+});
---
+++ src/data/socialLinks.ts
@@ -0,0 +1,5 @@
+const socialLinks = [
+  {
+    name: "LinkedIn",
+  },
+];
