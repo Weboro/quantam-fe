6d4fa63d5de7d61b8771e94b6d7c13c509359b3a
sayujkuickel <sayujkuickel@gmail.com>
2025-08-01 21:11:37 +0545

    feat: Add prettier config

---
+++ prettier.config.ts
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  embeddedLanguageFormatting: "auto",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
