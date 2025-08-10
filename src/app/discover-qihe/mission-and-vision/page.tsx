af2065b407b1fbd8dafb2a05e4de19bc4b47c7ce
Krishnna Devkota <krishdeeves2005@gmail.com>
2025-08-10 11:41:42 +0545

    animation:added animations on more pages

--- src/app/discover-qihe/mission-and-vision/page.tsx
+++ src/app/discover-qihe/mission-and-vision/page.tsx
@@ -3,8 +3,10 @@ import type { Metadata } from "next";
 import { SITE_TITLE, SITE_URL } from "@/extra/siteDetails";
 import { slugs } from "@/extra/slugs";
 //
+
 import ContainerLayout from "@/components/layouts/ContainerLayout";
 import Heading from "@/components/ui/Heading";
+import FadeUp from "@/components/animations/FadeUp";
 
 export const generateMetadata = (): Metadata => ({
   title: `Mission and Vision`,
@@ -38,45 +40,48 @@ const page = () => {
       </ContainerLayout>
 
       <ContainerLayout className=" grid md:grid-cols-2 gap-4">
-        <div className="border border-muted/50 shadow rounded-xl p-3">
-          <p className="w-22 aspect-square bg-brand-primary/10 rounded-full grid place-items-center">
-            <i
-              className={`fi fi-rr-bullseye-arrow flex text-5xl text-brand-primary`}
-            />
-          </p>
+        <FadeUp>
+          <div className="border border-muted/50 shadow rounded-xl p-3">
+            <p className="w-22 aspect-square bg-brand-primary/10 rounded-full grid place-items-center">
+              <i
+                className={`fi fi-rr-bullseye-arrow flex text-5xl text-brand-primary`}
+              />
+            </p>
 
-          <Heading className="text-brand-primary-black mt-4 mb-2" level={2}>
-            Our Mission
-          </Heading>
-          <p className="text-muted">
-            QIHE provides an exceptional higher education experience for
-            students from all walks of life. We achieve this by fostering a
-            dynamic and creative learning environment where education is an
-            active and lifelong journey. Our mission is to prepare students to
-            make meaningful contributions to their chosen professions and to
-            society.
-          </p>
-        </div>
+            <Heading className="text-brand-primary-black mt-4 mb-2" level={2}>
+              Our Mission
+            </Heading>
+            <p className="text-muted">
+              QIHE provides an exceptional higher education experience for
+              students from all walks of life. We achieve this by fostering a
+              dynamic and creative learning environment where education is an
+              active and lifelong journey. Our mission is to prepare students to
+              make meaningful contributions to their chosen professions and to
+              society.
+            </p>
+          </div>
+        </FadeUp>
+        <FadeUp delay={0.1}>
+          <div className="border border-muted/50 shadow rounded-xl p-3">
+            <p className="w-22 aspect-square bg-brand-primary/10 rounded-full grid place-items-center">
+              <i
+                className={`fi fi-rr-binoculars flex text-5xl text-brand-primary`}
+              />
+            </p>
 
-        <div className="border border-muted/50 shadow rounded-xl p-3">
-          <p className="w-22 aspect-square bg-brand-primary/10 rounded-full grid place-items-center">
-            <i
-              className={`fi fi-rr-binoculars flex text-5xl text-brand-primary`}
-            />
-          </p>
-
-          <Heading className="text-brand-primary-black mt-4 mb-2" level={2}>
-            Our Vision
-          </Heading>
-          <p className="text-muted">
-            At QIHE, we aspire to transform students lives and open doors to
-            professional opportunities through the delivery of quality higher
-            education. Our vision is to create an environment that fosters
-            intellectual curiosity, critical thinking, and the cultivation of
-            practical skills and humane values, empowering students to thrive in
-            their personal and professional lives.
-          </p>
-        </div>
+            <Heading className="text-brand-primary-black mt-4 mb-2" level={2}>
+              Our Vision
+            </Heading>
+            <p className="text-muted">
+              At QIHE, we aspire to transform students lives and open doors to
+              professional opportunities through the delivery of quality higher
+              education. Our vision is to create an environment that fosters
+              intellectual curiosity, critical thinking, and the cultivation of
+              practical skills and humane values, empowering students to thrive in
+              their personal and professional lives.
+            </p>
+          </div>
+        </FadeUp>
       </ContainerLayout>
     </>
   );
