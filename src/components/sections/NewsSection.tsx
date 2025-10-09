ef4a19d051426a1b770ceaf34e564a8142d1b240
hydrazin60 <sureshkhadka45@gmail.com>
2025-10-09 21:49:22 +0545

    final !!!

--- src/components/sections/NewsSection.tsx
+++ src/components/sections/NewsSection.tsx
@@ -12,20 +12,34 @@ const NewsSection = ({ showAll = false }) => (
     <ContainerLayout className="grid md:grid-cols-2 gap-4 mt-4 mb-12">
       <div className="flex flex-col items-between gap-2">
         <Heading level={2}>News & Articles</Heading>
-
         <Link href={slugs.news} className="font-semibold">
           <Button variant="secondary">More Blogs</Button>
         </Link>
       </div>
+
       <div className="flex items-end">
         <p className="text-muted ">
           Stay informed with the latest news, announcements, and achievements
           from the QIHE community, helping you remain engaged and connected with
           campus life and academic developments.
-          {/*  */}
         </p>
       </div>
     </ContainerLayout>
+
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+      {news.slice(0, showAll ? news.length : 3).map((card, index) => (
+        <FadeUp key={card.id} delay={index * 0.1}>
+          <NewsCard
+            imageUrl={card.imageUrl}
+            date={card.date}
+            readTime={card.readTime}
+            title={card.title}
+            summary={card.summary}
+            url={`/${slugs.news}/${card.slug}`}
+          />
+        </FadeUp>
+      ))}
+    </div>
   </ContainerLayout>
 );
 
